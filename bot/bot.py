a4af2b8bd7f9087d359f5b8707527fe3fb5a0ba5
flyboy1565 <flyboy1565@mail.com>
2021-07-08 10:29:27 -0700

    Added some changes

--- bot/bot.py
+++ bot/bot.py
@@ -4,6 +4,7 @@ import socket
 import command
 import requests
 import pyttsx3
+import json
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -32,6 +33,8 @@ class Bot():
         self.text_commands = self.reload_text_commands()
         self.channel = self.get_channels_to_monitor()
         self.TwitchBaseUrl = "https://api.twitch.tv/helix/"
+        self.message_trigger = 0
+        self.current_monitoring = []
 
     @staticmethod
     def bot_say(text):
@@ -61,14 +64,15 @@ class Bot():
         self.irc = socket.socket()
         self.irc.connect((self.server, self.port))
         self.irc_command(f"PASS oauth:{self.oauth_token}")
-        self.irc_command(f"NICK flyboy1565")
+        self.irc_command(f"NICK {self.bot_name}")
         for channel in self.channel:
             is_live = self.check_if_channel_is_live(channel)
-            if is_live is not None:
+            if is_live is not None and channel not in self.current_monitoring:
                 text = f'{channel} is live and started at {is_live["started_at"]}'
                 print(text)
-                # self.bot_say(text)
-            self.irc_command(f"JOIN #{channel}")
+                self.current_monitoring.append(channel)
+                self.irc_command(f"JOIN #{channel}")
+                self.send_message(channel, "Hello All!")
         self.irc_command(f"CAP REQ :twitch.tv/tags")        
         self.check_for_messages()
 
@@ -96,14 +100,16 @@ class Bot():
 
     
     # execute IRC commands
+    # execute IRC commands
     def irc_command(self, command: str):
-        # print(f'IRC - {command} :', )
+        print(f"IRC Commands -- {command}\r\n")
         self.irc.send((command + "\r\n").encode())
 
 
     # send privmsg's, which are normal chat messages
     def send_message(self, channel: str, message: str):
         self.irc_command(f"PRIVMSG #{channel} :{message}")
+
 
 
     # decode incoming messages
@@ -131,7 +137,7 @@ class Bot():
                 return
             
             regex = """@badge-info=(?P<badge_info>([\w\/\d\0]+|));badges=(?P<badges>([\w\/\d,]{1,})|);.*;display-name=(?P<display_name>[\w]+);emotes=(?P<emotes>([\w\/\d:\-]+)|).*id=(?P<message_id>[\w\-\d]+);mod=(?P<mod>\d+);.*room-id=(?P<room_id>\d+);subscriber=(?P<subscriber>\d+);.*user-id=(?P<user_id>\d+);.*PRIVMSG\s#(?P<room_name>[\w\-\d]+)\s:(?P<message>[@#\-\$A-Za-z0-9].*[^;])$"""
-
+            # print(message)
             pat_message = re.compile(regex, flags=re.IGNORECASE)
 
             details = pat_message.search(message)
@@ -149,22 +155,26 @@ class Bot():
                 self.store_message_data(user, room, d['message'], message_id)
                 return
 
-            if message.lower().startswith("ping"):
-                print('PING RECEIVED 1:')
-                self.irc_command("PONG")
-                return 
             user = self.get_user(details['user_id'], details['display_name'])
             room = self.get_room(details['room_name'].strip('#'), int(details['room_id']))
             print(f'Message: {details["room_name"]} - {details["display_name"]}: {details["message"]}\n')
+            message = details['message']
+            self.message_trigger += 1
             # check for commands being used
-            if message.startswith("{self.bot_trigger}"):
-                command = details['message'].split()[0].lower()
+            check = message.startswith(f"{self.bot_trigger}")
+            if message.startswith(f"{self.bot_trigger}"):
+                command = details['message'].replace(self.bot_trigger, '').split()[0].lower()
+                print('TextCommand Request: {} by user {}'.format(command, user))
                 if command not in self.text_commands and command not in self.commands:
                     self.store_wrong_command(user, command)
                 else:
-                    self.execute_command(user['username'], command, message)
+                    self.execute_command(details['user_id'], details['room_name'], command, message)
             
             self.store_message_data(user, room, details['message'], details['message_id'])
+            if self.message_trigger == 15:
+                self.reload_text_commands()
+                self.connect_to_channel()
+                self.message_trigger = 0 
 
         except AttributeError:
             pass
@@ -212,9 +222,6 @@ class Bot():
             response = requests.post(f'{DJANGO_URL}/rooms/', data=data)
             if response.status_code < 300:
                 return response.json()['id']
-            # print(response.request.url)
-            # print(response.request.body)
-            # print(response.request.headers)
             raise BaseException(f'Failed to create a room {room_name}: ', response.status_code)
         
 
@@ -236,15 +243,15 @@ class Bot():
 
     
     # insert data to SQLite db
-    def store_command_data(self, user: str, command: str, is_custom: int):
-        params = {'user':user['user_id'],'command': command, 'is_custom': bool(is_custom)}
-        response = requests.post(f'{DJANGO_URL}/command-usage/', data=params)
+    def store_command_data(self, user: int, command: str, is_custom: int):
+        data={"user":user , "command": command, "is_custom": bool(is_custom)}
+        response = requests.post(f'{DJANGO_URL}/command-usage/', data=data)
         if response.status_code> 300:
-            raise BaseException('Command failed to submit: {}'.format(params))
+            raise BaseException('Command failed to submit: {}'.format(data))
 
    
     # execute each command
-    def execute_command(self, user: str, command: str, message: str):
+    def execute_command(self, user: int, channel:str, command: str, message: str):
         # execute hard-coded command
         if command in self.commands.keys():
             self.commands[command].execute(user, message) 
@@ -253,8 +260,10 @@ class Bot():
 
         # execute custom text commands
         elif command in self.text_commands.keys():
+            print('Request to send Commnd: {}'.format(command))
+            print('Response in Channel {}: {}'.format(channel, self.text_commands[command]))
             self.send_message(
-                self.channel,
+                channel,
                 self.text_commands[command]
             )
             is_custom_command = 1
@@ -264,14 +273,9 @@ class Bot():
 
 
     def reload_text_commands(self):
-        from subprocess import getoutput
-        # print(f'URL : {DJANGO_URL}')
-        # print(f'URL : {DJANGO_URL}/text-commands/')
-        # print(getoutput(f'curl {DJANGO_URL}'))
-        # print()
         response = requests.get(f'{DJANGO_URL}/text-commands/')
         if response.status_code> 300:
             raise BaseException(f'Fail to get commands. {response.request.url} - {response.status_code}')
         raw_commands = response.json()
-        commands = {i['commands']: i['message'] for i in raw_commands}
+        commands = {i['command']: i['message'] for i in raw_commands}
         return commands
